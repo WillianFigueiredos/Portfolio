84154b68e02dd9fbb4ceabf5aebc0e65afc17c5b
Willian Figueiredo <williansfigueiredos@hotmail.com>
2023-01-24 15:13:57 -0300

    modf Card Projetos, add xp e certifi.

--- js/index.js
+++ js/index.js
@@ -5,3 +5,18 @@ document.getElementById("drop").addEventListener("click", function(){
 document.getElementById("droptoggle").addEventListener("click", function(){
     $("#myModal").modal("show");
 });
+
+
+document.getElementById("education-btn").addEventListener("click", function() {
+    document.getElementById("education").style.display = "block";
+    document.getElementById("experience").style.display = "none";
+  });
+  
+  document.getElementById("experience-btn").addEventListener("click", function() {
+    document.getElementById("education").style.display = "none";
+    document.getElementById("experience").style.display = "block";
+  });
+
+  document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("experience").style.display = "none";
+  });
